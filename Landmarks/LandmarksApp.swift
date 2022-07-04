a252f77116dae0f891272572aad3c0cd651aabc7
Fuki Sato <lego.sato.4135@gmail.com>
2022-07-04 21:13:19 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 佐藤風輝 on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
